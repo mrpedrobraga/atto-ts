4e60392b37f1feef9f9f0746046306da9338c614
Pedro Braga <mrhenribraga@gmail.com>
2024-04-10 08:44:21 -0300

    Changed things that are being exported.

--- index.ts
+++ index.ts
@@ -1,8 +1,4 @@
-import { Sx } from "src/prelude";
-
-function App() {
-    let todos = Sx.stateList([ 1, 2, 3 ]);
-    let todosString = todos.map(x => x.concat(4, 5, 6));
-}
-
-App();
+export import * as prelude from "./src/prelude";
+export import * as signal from "./src/element";
+export import * as signal_list from "./src/signal_list";
+export import * as element from "./element";
