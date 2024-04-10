b5ccdbfd0c780ba270c06aa3bdfc2b0719057bc7
Pedro Braga <mrhenribraga@gmail.com>
2024-04-10 11:50:50 +0000

    Corrected exporting

--- index.ts
+++ index.ts
@@ -1,4 +1,8 @@
-export import * as prelude from "./src/prelude";
-export import * as signal from "./src/element";
-export import * as signal_list from "./src/signal_list";
-export import * as element from "./element";
+import * as prelude from "./src/prelude";
+import * as signal from "./src/element";
+import * as signal_list from "./src/signal_list";
+import * as element from "./src/element";
+
+export default {
+    prelude, signal, signal_list, element
+}
